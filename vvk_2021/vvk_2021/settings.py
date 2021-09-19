5447868a67993cf8523029b6404226e739678b15
geomer198 <geomer198@gmail.com>
2021-09-19 23:14:23 +0300

    Change env config

--- vvk_2021/vvk_2021/settings.py
+++ vvk_2021/vvk_2021/settings.py
@@ -18,15 +18,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
 environ.Env.read_env(BASE_DIR / ".env")
-print(BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY", "TEST SECRET")
+SECRET_KEY = env("SECRET_KEY", default="TEST SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG", False)
+DEBUG = env("DEBUG", default=False)
 
 ALLOWED_HOSTS = ["*"]
 
